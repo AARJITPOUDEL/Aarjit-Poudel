109b9bfba8b56f9f8d8b82437a1cffc714ece101
Aarjit <98299656+AARJITPOUDEL@users.noreply.github.com>
2022-07-27 11:48:01 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+//by Aarjit Poudel😎😎😎😎😎😎
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+    
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+      
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,25 +16,25 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
+  
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+        
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+      
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
+ 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
+  
     var typed = new Typed(".typing", {
         strings: ["Programmer", "Web Developer", "Editor", "Designer", "Freelancer"],
         typeSpeed: 100,
@@ -48,7 +49,7 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousel script
+    
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
@@ -71,3 +72,4 @@ $(document).ready(function(){
         }
     });
 });
+//by by Aarjit Poudel😎😎😎😎😎😎
